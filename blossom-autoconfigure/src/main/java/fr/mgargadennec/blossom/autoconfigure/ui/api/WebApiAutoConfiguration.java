8ea8198dff3ea95bc44dbb3de80b0f50e3d5c2b8
Maël Gargadennec <mael.gargadennec@gmail.com>
2017-05-14 17:26:24 +0200

    Cache manager

--- blossom-autoconfigure/src/main/java/fr/mgargadennec/blossom/autoconfigure/ui/api/WebApiAutoConfiguration.java
+++ blossom-autoconfigure/src/main/java/fr/mgargadennec/blossom/autoconfigure/ui/api/WebApiAutoConfiguration.java
@@ -1,16 +1,20 @@
 package fr.mgargadennec.blossom.autoconfigure.ui.api;
 
-import fr.mgargadennec.blossom.ui.api.BuildInfoApiController;
+import fr.mgargadennec.blossom.ui.api.HomeApiController;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Maël Gargadennnec on 04/05/2017.
  */
 @Configuration
-@ConditionalOnClass(BuildInfoApiController.class)
+@ConditionalOnClass(HomeApiController.class)
 public class WebApiAutoConfiguration {
 
-
+    @Bean
+    public HomeApiController homeApiController() {
+        return new HomeApiController();
+    }
 
 }
