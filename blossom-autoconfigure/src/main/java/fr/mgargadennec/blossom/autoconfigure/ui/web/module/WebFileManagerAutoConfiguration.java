3d1790d4fe3b04b8e505dc6cd796da41944c3326
lperrod <perrod.louis@gmail.com>
2017-07-19 13:02:50 +0200

    Possibilite de disable un module
    Cle sous le namespace:
    blossom.filemanager.enabled
    Impossible de lire un fichier de properties lors de la configuration,
    donc si clé non présente, true par défaut.

--- blossom-autoconfigure/src/main/java/fr/mgargadennec/blossom/autoconfigure/ui/web/module/WebFileManagerAutoConfiguration.java
+++ blossom-autoconfigure/src/main/java/fr/mgargadennec/blossom/autoconfigure/ui/web/module/WebFileManagerAutoConfiguration.java
@@ -1,5 +1,14 @@
 package fr.mgargadennec.blossom.autoconfigure.ui.web.module;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
 import fr.mgargadennec.blossom.autoconfigure.core.CommonAutoConfiguration;
 import fr.mgargadennec.blossom.core.common.search.SearchEngineImpl;
 import fr.mgargadennec.blossom.module.filemanager.FileDTO;
@@ -8,22 +17,15 @@ import fr.mgargadennec.blossom.ui.menu.MenuItem;
 import fr.mgargadennec.blossom.ui.menu.MenuItemBuilder;
 import fr.mgargadennec.blossom.ui.web.content.filemanager.FileController;
 import fr.mgargadennec.blossom.ui.web.content.filemanager.FileManagerController;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 /**
  * Created by Maël Gargadennnec on 19/05/2017.
  */
 @Configuration
 @ConditionalOnBean(FileService.class)
+@ConditionalOnProperty(name = "blossom.filemanager.enabled", matchIfMissing = true)
 @AutoConfigureAfter(CommonAutoConfiguration.class)
 public class WebFileManagerAutoConfiguration {
-
 
   @Bean
   @Order(3)
@@ -33,8 +35,10 @@ public class WebFileManagerAutoConfiguration {
   }
 
   @Bean
-  public MenuItem contentFileManagerMenuItem(MenuItemBuilder builder, @Qualifier("contentMenuItem") MenuItem contentMenuItem) {
-    return builder.key("filemanager").label("menu.content.filemanager", true).link("/blossom/content/filemanager").icon("fa fa-photo").order(0).parent(contentMenuItem).build();
+  public MenuItem contentFileManagerMenuItem(MenuItemBuilder builder,
+      @Qualifier("contentMenuItem") MenuItem contentMenuItem) {
+    return builder.key("filemanager").label("menu.content.filemanager", true).link("/blossom/content/filemanager")
+        .icon("fa fa-photo").order(0).parent(contentMenuItem).build();
   }
 
   @Bean
