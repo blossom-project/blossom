f25c12949cbf9730f8f39e6b2347d24570701d3a
lperrod <perrod.louis@gmail.com>
2017-07-24 15:08:52 +0200

    Groups CRUD

--- blossom-core/blossom-core-group/src/main/java/fr/mgargadennec/blossom/core/group/GroupServiceImpl.java
+++ blossom-core/blossom-core-group/src/main/java/fr/mgargadennec/blossom/core/group/GroupServiceImpl.java
@@ -1,19 +1,50 @@
 package fr.mgargadennec.blossom.core.group;
 
-import fr.mgargadennec.blossom.core.common.mapper.DTOMapper;
-import fr.mgargadennec.blossom.core.common.service.GenericCrudServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEventPublisher;
+
+import fr.mgargadennec.blossom.core.common.event.CreatedEvent;
+import fr.mgargadennec.blossom.core.common.event.UpdatedEvent;
+import fr.mgargadennec.blossom.core.common.mapper.DTOMapper;
+import fr.mgargadennec.blossom.core.common.service.GenericCrudServiceImpl;
 
 /**
  * Created by Maël Gargadennnec on 03/05/2017.
  */
 public class GroupServiceImpl extends GenericCrudServiceImpl<GroupDTO, Group> implements GroupService {
-    private final static Logger logger = LoggerFactory.getLogger(GroupServiceImpl.class);
+  private static final Logger logger = LoggerFactory.getLogger(GroupServiceImpl.class);
 
-    public GroupServiceImpl(GroupDao dao, DTOMapper<Group, GroupDTO> mapper, ApplicationEventPublisher publisher) {
-        super(dao, mapper, publisher);
-    }
+  public GroupServiceImpl(GroupDao dao, DTOMapper<Group, GroupDTO> mapper, ApplicationEventPublisher publisher) {
+    super(dao, mapper, publisher);
+  }
+
+  @Override
+  public GroupDTO create(GroupCreateForm groupCreateForm) throws Exception {
+
+    Group groupToCreate = new Group();
+    groupToCreate.setName(groupCreateForm.getName());
+    groupToCreate.setDescription(groupCreateForm.getDescription());
+
+    GroupDTO savedgroup = this.mapper.mapEntity(this.dao.create(groupToCreate));
+
+    this.publisher.publishEvent(new CreatedEvent<GroupDTO>(this, savedgroup));
+
+    return savedgroup;
+  }
+
+  @Override
+  public GroupDTO update(Long groupId, GroupUpdateForm groupUpdateForm) throws Exception {
+
+    Group groupToUpdate = new Group();
+    groupToUpdate.setName(groupUpdateForm.getName());
+    groupToUpdate.setDescription(groupUpdateForm.getDescription());
+
+    GroupDTO savedgroup = this.mapper.mapEntity(this.dao.update(groupId, groupToUpdate));
+
+    this.publisher.publishEvent(new UpdatedEvent<GroupDTO>(this, savedgroup));
+
+    return savedgroup;
+  }
 
 }
--- blossom-ui/blossom-ui-web/src/main/java/fr/mgargadennec/blossom/ui/web/administration/group/GroupsController.java
+++ blossom-ui/blossom-ui-web/src/main/java/fr/mgargadennec/blossom/ui/web/administration/group/GroupsController.java
@@ -1,24 +1,34 @@
 package fr.mgargadennec.blossom.ui.web.administration.group;
 
-import com.google.common.base.Strings;
-import fr.mgargadennec.blossom.core.common.search.SearchEngineImpl;
-import fr.mgargadennec.blossom.core.group.GroupDTO;
-import fr.mgargadennec.blossom.core.group.GroupService;
-import fr.mgargadennec.blossom.ui.menu.OpenedMenu;
-import fr.mgargadennec.blossom.ui.stereotype.BlossomController;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.util.NoSuchElementException;
+import com.google.common.base.Strings;
+
+import fr.mgargadennec.blossom.core.common.search.SearchEngineImpl;
+import fr.mgargadennec.blossom.core.group.GroupCreateForm;
+import fr.mgargadennec.blossom.core.group.GroupDTO;
+import fr.mgargadennec.blossom.core.group.GroupService;
+import fr.mgargadennec.blossom.core.group.GroupUpdateForm;
+import fr.mgargadennec.blossom.ui.menu.OpenedMenu;
+import fr.mgargadennec.blossom.ui.stereotype.BlossomController;
 
 /**
  * Created by Maël Gargadennnec on 05/05/2017.
@@ -27,101 +37,121 @@ import java.util.NoSuchElementException;
 @OpenedMenu("groups")
 public class GroupsController {
 
-    private static final Logger logger = LoggerFactory.getLogger(GroupsController.class);
+  private static final Logger logger = LoggerFactory.getLogger(GroupsController.class);
 
-    private final GroupService groupService;
-    private final SearchEngineImpl<GroupDTO> searchEngine;
+  private final GroupService groupService;
+  private final SearchEngineImpl<GroupDTO> searchEngine;
 
-    public GroupsController(GroupService groupService, SearchEngineImpl<GroupDTO> searchEngine) {
-        this.groupService = groupService;
-        this.searchEngine = searchEngine;
+  public GroupsController(GroupService groupService, SearchEngineImpl<GroupDTO> searchEngine) {
+    this.groupService = groupService;
+    this.searchEngine = searchEngine;
+  }
+
+  @GetMapping
+  public ModelAndView getGroupsPage(@RequestParam(value = "q", required = false) String q,
+      @PageableDefault(size = 25) Pageable pageable, Model model) {
+    return tableView(q, pageable, model, "groups/groups");
+  }
+
+  @GetMapping("/_list")
+  public ModelAndView getGroupsTable(@RequestParam(value = "q", required = false) String q,
+      @PageableDefault(size = 25) Pageable pageable, Model model) {
+    return tableView(q, pageable, model, "groups/table");
+  }
+
+  private ModelAndView tableView(String q, Pageable pageable, Model model, String viewName) {
+    Page<GroupDTO> groups;
+
+    if (Strings.isNullOrEmpty(q)) {
+      groups = this.groupService.getAll(pageable);
+    } else {
+      groups = this.searchEngine.search(q, pageable).getPage();
     }
 
-    @GetMapping
-    public ModelAndView getGroupsPage(@RequestParam(value = "q", required = false) String q, @PageableDefault(size = 25) Pageable pageable, Model model) {
-        return tableView(q, pageable, model, "groups/groups");
+    model.addAttribute("groups", groups);
+    model.addAttribute("q", q);
+
+    return new ModelAndView(viewName, model.asMap());
+  }
+
+  @GetMapping("/_create")
+  public ModelAndView getGroupCreatePage(Model model, Locale locale) {
+    GroupCreateForm groupCreateForm = new GroupCreateForm();
+    groupCreateForm.setLocale(locale);
+    return this.createView(groupCreateForm, model);
+  }
+
+  @PostMapping("/_create")
+  public ModelAndView handleGroupCreateForm(@Valid @ModelAttribute("groupCreateForm") GroupCreateForm groupCreateForm,
+      BindingResult bindingResult, Model model) {
+    if (bindingResult.hasErrors()) {
+      return this.createView(groupCreateForm, model);
+    }
+    try {
+      GroupDTO group = this.groupService.create(groupCreateForm);
+      return new ModelAndView("redirect:../groups/" + group.getId());
+    } catch (Exception e) {
+      logger.error("Error on creating group, name " + groupCreateForm.getName() + " already exists ", e);
+      return this.createView(groupCreateForm, model);
     }
 
-    @GetMapping("/_list")
-    public ModelAndView getGroupsTable(@RequestParam(value = "q", required = false) String q, @PageableDefault(size = 25) Pageable pageable, Model model) {
-        return tableView(q, pageable, model, "groups/table");
+  }
+
+  private ModelAndView createView(GroupCreateForm groupCreateForm, Model model) {
+    model.addAttribute("groupCreateForm", groupCreateForm);
+    return new ModelAndView("groups/create", model.asMap());
+  }
+
+  @GetMapping("/{id}")
+  public ModelAndView getGroup(@PathVariable Long id, Model model, HttpServletRequest request) {
+    GroupDTO group = this.groupService.getOne(id);
+    if (group == null) {
+      throw new NoSuchElementException(String.format("Group=%s not found", id));
+    }
+    model.addAttribute("group", group);
+    return new ModelAndView("groups/group", "group", group);
+  }
+
+  @PostMapping("/{id}/_delete")
+  public String deleteGroup(@PathVariable Long id) {
+    this.groupService.delete(this.groupService.getOne(id));
+    return "redirect:..";
+  }
+
+  @GetMapping("/{id}/_edit")
+  public ModelAndView getGroupUpdatePage(@PathVariable Long id, Model model, Locale locale) {
+    GroupDTO group = this.groupService.getOne(id);
+    if (group == null) {
+      throw new NoSuchElementException(String.format("Group=%s not found", id));
     }
 
-    private ModelAndView tableView(String q, Pageable pageable, Model model, String viewName) {
-        Page<GroupDTO> groups;
+    return this.editView(group, locale);
+  }
 
-        if (Strings.isNullOrEmpty(q)) {
-            groups = this.groupService.getAll(pageable);
-        } else {
-            groups = this.searchEngine.search(q, pageable).getPage();
-        }
-
-        model.addAttribute("groups", groups);
-        model.addAttribute("q", q);
-
-        return new ModelAndView(viewName, model.asMap());
+  @PostMapping("/{id}/_edit")
+  public ModelAndView handleUpdateGroupForm(@PathVariable Long id,
+      @Valid @ModelAttribute("groupUpdateForm") GroupUpdateForm groupUpdateForm, BindingResult bindingResult,
+      Locale locale) {
+    if (bindingResult.hasErrors()) {
+      return this.editView(groupUpdateForm, locale);
+    }
+    try {
+      this.groupService.update(id, groupUpdateForm);
+      return new ModelAndView("redirect:../" + id);
+    } catch (Exception e) {
+      return this.editView(groupUpdateForm, locale);
     }
 
-    @GetMapping("/_create")
-    public ModelAndView getGroupCreatePage() {
-        return this.createView(new GroupDTO());
-    }
+  }
 
-    @PostMapping("/_create")
-    public ModelAndView handleGroupCreateForm(@Valid @ModelAttribute("group") GroupDTO group) {
-        try {
-            group = this.groupService.create(group);
-        } catch (DataIntegrityViolationException e) {
-            logger.error("Error on creating group, name " + group.getName() + " already exists ", e);
-            return this.createView(group);
-        }
-        return new ModelAndView("redirect:/groups/" + group.getId());
-    }
+  private ModelAndView editView(GroupUpdateForm groupUpdateForm, Locale locale) {
+    groupUpdateForm.setLocale(locale);
+    return new ModelAndView("groups/update", "groupUpdateForm", groupUpdateForm);
+  }
 
-    private ModelAndView createView(GroupDTO group) {
-        return new ModelAndView("groups/create", "group", group);
-    }
+  private ModelAndView editView(GroupDTO group, Locale locale) {
+    GroupUpdateForm groupUpdateForm = new GroupUpdateForm(group.getName(), group.getDescription(), locale);
+    return new ModelAndView("groups/update", "groupUpdateForm", groupUpdateForm);
+  }
 
-
-    @GetMapping("/{id}")
-    public ModelAndView getGroup(@PathVariable Long id, Model model, HttpServletRequest request) {
-        GroupDTO group = this.groupService.getOne(id);
-        if (group == null) {
-            throw new NoSuchElementException(String.format("Group=%s not found", id));
-        }
-        model.addAttribute("group", group);
-        return new ModelAndView("groups/group", "group", group);
-    }
-
-
-    @PostMapping("/{id}/_delete")
-    public String deleteGroup(@PathVariable Long id) {
-        this.groupService.delete(this.groupService.getOne(id));
-        return "redirect:/groups";
-    }
-
-    @GetMapping("/{id}/_edit")
-    public ModelAndView getGroupUpdatePage(@PathVariable Long id, Model model) {
-        GroupDTO group = this.groupService.getOne(id);
-        if (group == null) {
-            throw new NoSuchElementException(String.format("Group=%s not found", id));
-        }
-
-        return this.editView(group);
-    }
-
-    @PostMapping("/{id}/_edit")
-    public ModelAndView handleUpdateGroupForm(@PathVariable Long id, @Valid @ModelAttribute("group") GroupDTO group) {
-        try {
-            this.groupService.update(id, group);
-        } catch (Exception e) {
-            return this.editView(group);
-        }
-        return new ModelAndView("redirect:/groups/" + id);
-    }
-
-    private ModelAndView editView(GroupDTO group) {
-        return new ModelAndView("groups/update", "group", group);
-    }
 }
-
