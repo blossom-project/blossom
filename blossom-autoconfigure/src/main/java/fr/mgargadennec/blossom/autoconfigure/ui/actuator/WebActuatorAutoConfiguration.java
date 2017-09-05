e21acb8e3455be0a0dc19a853003faf5abeea102
lperrod <perrod.louis@gmail.com>
2017-09-05 14:20:31 +0200

    Correction déclaration fichier de properties

--- blossom-autoconfigure/src/main/java/fr/mgargadennec/blossom/autoconfigure/ui/actuator/WebActuatorAutoConfiguration.java
+++ blossom-autoconfigure/src/main/java/fr/mgargadennec/blossom/autoconfigure/ui/actuator/WebActuatorAutoConfiguration.java
@@ -1,18 +1,18 @@
 package fr.mgargadennec.blossom.autoconfigure.ui.actuator;
 
-import fr.mgargadennec.blossom.core.common.actuator.ElasticsearchTraceRepository;
-import fr.mgargadennec.blossom.core.common.actuator.TraceStatisticsEndpoint;
-import fr.mgargadennec.blossom.core.common.actuator.TraceStatisticsMvcEndpoint;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import fr.mgargadennec.blossom.core.common.actuator.TraceStatisticsEndpoint;
+import fr.mgargadennec.blossom.core.common.actuator.TraceStatisticsMvcEndpoint;
+
 /**
  * Created by Maël Gargadennnec on 11/05/2017.
  */
 @Configuration
-@PropertySource("actuator.properties")
+@PropertySource("classpath:/actuator.properties")
 public class WebActuatorAutoConfiguration {
 
   @Bean
