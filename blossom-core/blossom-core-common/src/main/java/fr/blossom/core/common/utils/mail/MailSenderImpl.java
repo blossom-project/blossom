d1520c826955a761a7d83ef4e64d68295a5289c2
RLejolivet <raphael.lejolivet@gmail.com>
2017-11-19 12:18:22 +0100

    Activation mail (#53)
    * Change logger wording
    
    * Fix mail templates (do not use ${name} before defining it)

--- blossom-core/blossom-core-common/src/main/java/fr/blossom/core/common/utils/mail/MailSenderImpl.java
+++ blossom-core/blossom-core-common/src/main/java/fr/blossom/core/common/utils/mail/MailSenderImpl.java
@@ -61,7 +61,7 @@ public class MailSenderImpl implements MailSender {
       this.javaMailSender.send(mimeMessage);
     } else {
       LOGGER
-        .info("A mail whose subject was {} was not sent because the recipient adresses were filtered by mailfilters {}",
+        .info("A mail with recipient(s) '{}' and subject '{}' was not sent because the recipient adresses were filtered by mailfilters {}",
           Arrays.toString(mailTo), mailSubject, this.filters);
     }
   }
