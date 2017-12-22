9d8ba79110480a7cc40e871fc338cd1a673aab7a
Maël Gargadennec <mael.gargadennec@gmail.com>
2017-12-22 12:41:13 +0100

    adding unit tests dependencies to modules

---
+++ blossom-core/blossom-core-group/src/test/java/fr/blossom/core/group/GroupServiceImplTest.java
@@ -0,0 +1,14 @@
+package fr.blossom.core.group;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GroupServiceImplTest {
+
+  @Test
+  public void test() {
+
+  }
+}
---
+++ blossom-core/blossom-core-validation/src/test/java/fr/blossom/core/validation/FieldMatchValidatorTest.java
@@ -0,0 +1,14 @@
+package fr.blossom.core.validation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FieldMatchValidatorTest {
+
+  @Test
+  public void test() throws Exception {
+
+  }
+}
