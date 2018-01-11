d87d11eddaec3e7b42c0adede36507a529a32ebb
Maël Gargadennec <mael.gargadennec@gmail.com>
2018-01-11 14:12:48 +0100

    Test fixing

--- blossom-core/blossom-core-common/src/test/java/fr/blossom/core/common/search/IndexationEngineImplTest.java
+++ blossom-core/blossom-core-common/src/test/java/fr/blossom/core/common/search/IndexationEngineImplTest.java
@@ -26,7 +26,9 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;;
+import org.mockito.junit.MockitoJUnitRunner;
+
+;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IndexationEngineImplTest {
