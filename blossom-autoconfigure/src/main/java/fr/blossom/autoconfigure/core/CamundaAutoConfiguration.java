f3a8ddd7b8678fb55eb6be2634f21166ec5152f7
Maël Gargadennec <mael.gargadennec@gmail.com>
2018-01-30 11:24:59 +0100

    Camunda autoconfiguration Signed-off-by: Maël Gargadennec <mael.gargadennec@gmail.com>

--- blossom-autoconfigure/src/main/java/fr/blossom/autoconfigure/core/CamundaAutoConfiguration.java
+++ blossom-autoconfigure/src/main/java/fr/blossom/autoconfigure/core/CamundaAutoConfiguration.java
@@ -2,8 +2,10 @@ package fr.blossom.autoconfigure.core;
 
 import org.camunda.bpm.spring.boot.starter.CamundaBpmAutoConfiguration;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.rest.CamundaBpmRestJerseyAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
@@ -13,8 +15,13 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @PropertySource("classpath:/camunda.properties")
 @ConditionalOnClass(CamundaBpmAutoConfiguration.class)
-@AutoConfigureBefore(CamundaBpmAutoConfiguration.class)
-@EnableProcessApplication
+@AutoConfigureBefore({HibernateJpaAutoConfiguration.class, CamundaBpmAutoConfiguration.class,
+  CamundaBpmRestJerseyAutoConfiguration.class})
 public class CamundaAutoConfiguration {
 
+  @Configuration
+  @EnableProcessApplication
+  public static class DefaultCamundaActivation {
+
+  }
 }
