9ff114518c7bc54680177c2b0a4795681c3cb10b
Maël Gargadennec <mael.gargadennec@gmail.com>
2018-02-08 15:58:29 +0100

    test Signed-off-by: Maël Gargadennec <mael.gargadennec@gmail.com>

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_b500254f5078_key -iv $encrypted_b500254f5078_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import cd/codesigning.asc
+    openssl aes-256-cbc -K $encrypted_4e3556a82943_key -iv $encrypted_4e3556a82943_iv -in codesigning.asc.enc -out codesigning.asc -d
+    gpg --fast-import codesigning.asc
 fi
