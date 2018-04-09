ccd6445c2232c5c34d1b2a79de06a17dae982251
Maël Gargadennec <mael.gargadennec@gmail.com>
2018-04-09 16:17:13 +0200

    Travis deploy Signed-off-by: Maël Gargadennec <mael.gargadennec@gmail.com>

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -2,3 +2,8 @@
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -e -P build-extras,sign --settings cd/mvnsettings.xml
 fi
+
+if [ "$TRAVIS_TAG" != '' ]; then
+    mvn versions:set -DnewVersion=${TRAVIS_TAG}
+    mvn deploy -e -P build-extras,sign --settings cd/mvnsettings.xml
+fi
