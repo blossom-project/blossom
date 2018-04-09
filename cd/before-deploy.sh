962630e49346f91a167d04f7c0c2d08b9d521a53
Maël Gargadennec <mael.gargadennec@gmail.com>
2018-04-09 16:49:39 +0200

    TravisCI correction

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if ([ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]) ||  ["$TRAVIS_TAG" != ''] ; then
+if ([ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]) ||  [ "$TRAVIS_TAG" != '' ] ; then
     openssl aes-256-cbc -K $encrypted_b4f84f3a0738_key -iv $encrypted_b4f84f3a0738_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import codesigning.asc
 fi
