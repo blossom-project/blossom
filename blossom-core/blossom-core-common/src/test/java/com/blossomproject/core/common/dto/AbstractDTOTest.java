98d84ec7df07ad4699706aea9c07c0569862691a
Raphaël Lejolivet <rlejolivet@cardiweb.com>
2018-04-26 16:34:59 +0200

    Add tests for AbstractDTO copy constructor

---
+++ blossom-core/blossom-core-common/src/test/java/com/blossomproject/core/common/dto/AbstractDTOTest.java
@@ -0,0 +1,59 @@
+package com.blossomproject.core.common.dto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AbstractDTOTest {
+
+  public static class TestDTO extends AbstractDTO {
+    public TestDTO() {
+    }
+
+    public TestDTO(TestDTO dto) {
+      super(dto);
+    }
+  }
+
+  @Test
+  public void should_copy_be_immutable() {
+    TestDTO aDTO = new TestDTO();
+    aDTO.setId(123L);
+    aDTO.setCreationDate(Date.from(Instant.now()));
+    aDTO.setCreationUser("Creation user");
+    aDTO.setModificationDate(Date.from(Instant.now()));
+    aDTO.setModificationUser("Modification user");
+
+    TestDTO anotherDTO = new TestDTO(aDTO);
+
+    assertEquals("Id should be the same for the copy", aDTO.getId(), anotherDTO.getId());
+    assertEquals("Creation date should be the same for the copy", aDTO.getCreationDate(), anotherDTO.getCreationDate());
+    assertEquals("Creation user should be the same for the copy", aDTO.getCreationUser(), anotherDTO.getCreationUser());
+    assertEquals("Modification date should be the same for the copy", aDTO.getModificationDate(), anotherDTO.getModificationDate());
+    assertEquals("Modification user should be the same for the copy", aDTO.getModificationUser(), anotherDTO.getModificationUser());
+
+    assertNotSame("Creation date object should be distinct", aDTO.getCreationDate(), anotherDTO.getCreationDate());
+    assertNotSame("Modification date object should be distinct", aDTO.getModificationDate(), anotherDTO.getModificationDate());
+
+    aDTO.setId(456L);
+    aDTO.setCreationDate(Date.from(Instant.now().plus(5, ChronoUnit.MINUTES)));
+    aDTO.setCreationUser("Another Creation user");
+    aDTO.setModificationDate(Date.from(Instant.now().plus(5, ChronoUnit.MINUTES)));
+    aDTO.setModificationUser("Another Modification user");
+
+    assertNotEquals("Id should be independent for the copy", aDTO.getId(), anotherDTO.getId());
+    assertNotEquals("Creation date should be independent for the copy", aDTO.getCreationDate(), anotherDTO.getCreationDate());
+    assertNotEquals("Creation user should be independent for the copy", aDTO.getCreationUser(), anotherDTO.getCreationUser());
+    assertNotEquals("Modification date should be independent for the copy", aDTO.getModificationDate(), anotherDTO.getModificationDate());
+    assertNotEquals("Modification user should be independent for the copy", aDTO.getModificationUser(), anotherDTO.getModificationUser());
+
+  }
+
+}
