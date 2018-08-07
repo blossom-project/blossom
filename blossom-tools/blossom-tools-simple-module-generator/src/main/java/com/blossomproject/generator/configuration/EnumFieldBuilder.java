ffb9060ade2b3399aa9c147bf89d67a52d59d845
Raphaël Lejolivet <rlejolivet@cardiweb.com>
2018-08-07 18:24:08 +0200

    Add varchar size to Enum generated entities

--- blossom-tools/blossom-tools-simple-module-generator/src/main/java/com/blossomproject/generator/configuration/EnumFieldBuilder.java
+++ blossom-tools/blossom-tools-simple-module-generator/src/main/java/com/blossomproject/generator/configuration/EnumFieldBuilder.java
@@ -1,7 +1,6 @@
 package com.blossomproject.generator.configuration;
 
 import com.blossomproject.generator.configuration.model.Field;
-import com.blossomproject.generator.configuration.model.impl.DefaultField;
 import com.blossomproject.generator.configuration.model.impl.EnumField;
 
 public class EnumFieldBuilder extends FieldBuilder<EnumFieldBuilder> {
@@ -10,13 +9,21 @@ public class EnumFieldBuilder extends FieldBuilder<EnumFieldBuilder> {
     private final Class<? extends Enum> enumClass;
 
     EnumFieldBuilder(FieldsBuilder parent, String name, Class<? extends Enum> enumClass) {
-        super(parent, name, enumClass, "varchar");
+        super(parent, name, enumClass, "varchar(" + maxSize(enumClass) + ")");
         this.enumClass = enumClass;
     }
 
     @Override
     Field build() {
         return new EnumField(name, columnName, className, jdbcType, required, updatable, nullable,
-                defaultValue, searchable, enumClass);
+                defaultValue, searchable, maxSize(enumClass));
+    }
+
+    private static int maxSize(Class<? extends Enum> enumClass) {
+        int maxSize = 0;
+        for (Enum enumValue : enumClass.getEnumConstants()) {
+            maxSize = Math.max(maxSize, enumValue.name().length());
+        }
+        return maxSize;
     }
 }
--- blossom-tools/blossom-tools-simple-module-generator/src/main/java/com/blossomproject/generator/configuration/model/impl/EnumField.java
+++ blossom-tools/blossom-tools-simple-module-generator/src/main/java/com/blossomproject/generator/configuration/model/impl/EnumField.java
@@ -1,11 +1,9 @@
 package com.blossomproject.generator.configuration.model.impl;
 
-public class EnumField extends  DefaultField {
+public class EnumField extends StringField {
 
-    private final Class<? extends Enum> enumClass;
+  public EnumField(String name, String columnName, Class<?> className, String jdbcType, boolean required, boolean updatable, boolean nullable, String defaultValue, boolean searchable, int maxLength) {
+    super(name, columnName, className, jdbcType, required, updatable, nullable, defaultValue, searchable, true, maxLength, false);
+  }
 
-    public EnumField(String name, String columnName, Class<?> className, String jdbcType, boolean required, boolean updatable, boolean nullable, String defaultValue, boolean searchable, Class<? extends Enum> enumClass) {
-        super(name, columnName, className, jdbcType, required, updatable, nullable, defaultValue, searchable);
-        this.enumClass = enumClass;
-    }
 }
